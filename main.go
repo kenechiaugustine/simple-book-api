700617c4a0cd0e4aa539db28b8d7b5f2bf20b07f
Kene Arionye <kenechiaugustine@gmail.com>
2022-10-07 03:25:22 +0100

    get all books func

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "net/http"
+
 type book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
@@ -11,4 +13,8 @@ var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
 }
